d552dce48cc8c67bb2078ddc845838daf5afc8e5
typxyr <typxyr@live.com>
2017-03-18 18:32:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,8 +60,8 @@ run sudo scutil --set LocalHostName "'$computer_name'"
 run sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "'$computer_name'"
 
 # UX And Performance Improvements
-echo "Disable sudden motion sensor. (Not useful for SSDs)."
-run sudo pmset -a sms 0
+#echo "Disable sudden motion sensor. (Not useful for SSDs)."
+#run sudo pmset -a sms 0
 
 echo "Disable press-and-hold for keys in favor of key repeat."
 run defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
@@ -70,20 +70,20 @@ echo "Set a fast keyboard repeat rate, after a good initial delay."
 run defaults write NSGlobalDomain KeyRepeat -int 1
 run defaults write NSGlobalDomain InitialKeyRepeat -int 25
 
-echo "Disable auto-correct."
-run defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+#echo "Disable auto-correct."
+#run defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 echo "Speed up mission control animations."
 run defaults write com.apple.dock expose-animation-duration -float 0.1
 
-echo "Remove the auto-hiding dock delay."
-run defaults write com.apple.dock autohide-delay -int 0
+#echo "Remove the auto-hiding dock delay."
+#run defaults write com.apple.dock autohide-delay -int 0
 
 echo "Use the dark theme."
 run defaults write ~/Library/Preferences/.GlobalPreferences AppleInterfaceStyle -string "Dark"
 
-echo "Save screenshots in PNG format."
-run defaults write com.apple.screencapture type -string png
+#echo "Save screenshots in PNG format."
+#run defaults write com.apple.screencapture type -string png
 
 echo "Save screenshots to user screenshots directory instead of desktop."
 run mkdir ~/screenshots
@@ -127,99 +127,99 @@ run defaults write _FXShowPosixPathInTitle com.apple.finder -int 1
 echo "Don't write DS_Store files to network shares."
 run defaults write DSDontWriteNetworkStores com.apple.desktopservices -int 1
 
-echo "Don't ask to use external drives as a Time Machine backup."
-run defaults write DoNotOfferNewDisksForBackup com.apple.TimeMachine -int 1
+#echo "Don't ask to use external drives as a Time Machine backup."
+#run defaults write DoNotOfferNewDisksForBackup com.apple.TimeMachine -int 1
 
-echo "Disable natural scrolling."
-run defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false
+#echo "Disable natural scrolling."
+#run defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false
 
 # Security And Privacy Improvements
-echo "Disable Safari from auto-filling sensitive data."
-run defaults write ~/Library/Preferences/com.apple.Safari AutoFillCreditCardData -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari AutoFillFromAddressBook -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari AutoFillMiscellaneousForms -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari AutoFillPasswords -bool false
+#echo "Disable Safari from auto-filling sensitive data."
+#run defaults write ~/Library/Preferences/com.apple.Safari AutoFillCreditCardData -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari AutoFillFromAddressBook -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari AutoFillMiscellaneousForms -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari AutoFillPasswords -bool false
 
-echo "Disable Safari from automatically opening files."
-run defaults write ~/Library/Preferences/com.apple.Safari AutoOpenSafeDownloads -bool false
+#echo "Disable Safari from automatically opening files."
+#run defaults write ~/Library/Preferences/com.apple.Safari AutoOpenSafeDownloads -bool false
 
-echo "Always block cookies and local storage in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari BlockStoragePolicy -bool false
+#echo "Always block cookies and local storage in Safari."
+#run defaults write ~/Library/Preferences/com.apple.Safari BlockStoragePolicy -bool false
 
-echo "Enable Safari warnings when visiting fradulent websites."
-run defaults write ~/Library/Preferences/com.apple.Safari WarnAboutFraudulentWebsites -bool true
+#echo "Enable Safari warnings when visiting fradulent websites."
+#run defaults write ~/Library/Preferences/com.apple.Safari WarnAboutFraudulentWebsites -bool true
 
-echo "Disable javascript in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaScriptEnabled -bool false
+#echo "Disable javascript in Safari."
+#run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptEnabled -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaScriptEnabled -bool false
 
-echo "Block popups in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
+#echo "Block popups in Safari."
+#run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
 
-echo "Disable plugins and extensions in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2WebGLEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2PluginsEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitPluginsEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari ExtensionsEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari PlugInFirstVisitPolicy PlugInPolicyBlock
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaEnabled -bool false
+#echo "Disable plugins and extensions in Safari."
+#run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2WebGLEnabled -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2PluginsEnabled -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari WebKitPluginsEnabled -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari ExtensionsEnabled -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari PlugInFirstVisitPolicy PlugInPolicyBlock
+#run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
+#run defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaEnabled -bool false
 
-echo "Safari should treat SHA-1 certificates as insecure."
-run defaults write ~/Library/Preferences/com.apple.Safari TreatSHA1CertificatesAsInsecure -bool true
+#echo "Safari should treat SHA-1 certificates as insecure."
+#run defaults write ~/Library/Preferences/com.apple.Safari TreatSHA1CertificatesAsInsecure -bool true
 
 echo "Disable pre-loading websites with high search rankings."
 run defaults write ~/Library/Preferences/com.apple.Safari PreloadTopHit -bool false
 
-echo "Disable Safari search engine suggestions."
-run defaults write ~/Library/Preferences/com.apple.Safari SuppressSearchSuggestions -bool true
+#echo "Disable Safari search engine suggestions."
+#run defaults write ~/Library/Preferences/com.apple.Safari SuppressSearchSuggestions -bool true
 
-echo "Enable Do-Not-Track HTTP header in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari SendDoNotTrackHTTPHeader -bool true
+#echo "Enable Do-Not-Track HTTP header in Safari."
+#run defaults write ~/Library/Preferences/com.apple.Safari SendDoNotTrackHTTPHeader -bool true
 
-echo "Disable pdf viewing in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitOmitPDFSupport -bool true
+#echo "Disable pdf viewing in Safari."
+#run defaults write ~/Library/Preferences/com.apple.Safari WebKitOmitPDFSupport -bool true
 
 echo "Display full website addresses in Safari."
 run defaults write ~/Library/Preferences/com.apple.Safari ShowFullURLInSmartSearchField -bool true
 
-echo "Disable loading remote content in emails in Apple Mail."
-run defaults write ~/Library/Preferences/com.apple.mail-shared DisableURLLoading -bool true
+#echo "Disable loading remote content in emails in Apple Mail."
+#run defaults write ~/Library/Preferences/com.apple.mail-shared DisableURLLoading -bool true
 
-echo "Send junk mail to the junk mail box in Apple Mail."
-run defaults write ~/Library/Containers/com.apple.mail/Data/Library/Preferences/com.apple.mail JunkMailBehavior -int 2
+#echo "Send junk mail to the junk mail box in Apple Mail."
+#run defaults write ~/Library/Containers/com.apple.mail/Data/Library/Preferences/com.apple.mail JunkMailBehavior -int 2
 
-echo "Disable spotlight universal search (don't send info to Apple)."
-run defaults write com.apple.safari UniversalSearchEnabled -int 0
+#echo "Disable spotlight universal search (don't send info to Apple)."
+#run defaults write com.apple.safari UniversalSearchEnabled -int 0
 
-echo "Disable Spotlight Suggestions, Bing Web Search, and other leaky data."
-run python ./fix_leaky_data.py
+#echo "Disable Spotlight Suggestions, Bing Web Search, and other leaky data."
+#run python ./fix_leaky_data.py
 
-echo "Disable Captive Portal Hijacking Attack."
-run defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control Active -bool false
+#echo "Disable Captive Portal Hijacking Attack."
+#run defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control Active -bool false
 
-echo "Set screen to lock as soon as the screensaver starts."
-run defaults write com.apple.screensaver askForPassword -int 1
-run defaults write com.apple.screensaver askForPasswordDelay -int 0
+#echo "Set screen to lock as soon as the screensaver starts."
+#run defaults write com.apple.screensaver askForPassword -int 1
+#run defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-echo "Don't default to saving documents to iCloud."
-run defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+#echo "Don't default to saving documents to iCloud."
+#run defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-echo "Disable crash reporter."
-run defaults write com.apple.CrashReporter DialogType none
+#echo "Disable crash reporter."
+#run defaults write com.apple.CrashReporter DialogType none
 
-echo "Enable Stealth Mode. Computer will not respond to ICMP ping requests or connection attempts from a closed TCP/UDP port."
-run defaults write /Library/Preferences/com.apple.alf stealthenabled -bool true
+#echo "Enable Stealth Mode. Computer will not respond to ICMP ping requests or connection attempts from a closed TCP/UDP port."
+#run defaults write /Library/Preferences/com.apple.alf stealthenabled -bool true
 
-echo "Set all network interfaces to use Google DNS."
-run bash ./use_google_dns.sh
+#echo "Set all network interfaces to use Google DNS."
+#run bash ./use_google_dns.sh
 
-echo "Disable wake on network access."
-run systemsetup -setwakeonnetworkaccess off
+#echo "Disable wake on network access."
+#run systemsetup -setwakeonnetworkaccess off
 
-echo "Disable Bonjour multicast advertisements."
-run defaults write /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool YES
+#echo "Disable Bonjour multicast advertisements."
+#run defaults write /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool YES
 
 echo "Enable Mac App Store automatic updates."
 run defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
@@ -262,9 +262,9 @@ run brew install git
 echo "Install mas (Mac App Store Command Line)."
 run brew install mas
 
-echo "Install youtube-dl."
-run brew install youtube-dl
-run brew install ffmpeg
+#echo "Install youtube-dl."
+#run brew install youtube-dl
+#run brew install ffmpeg
 
 # Trust a curl | bash? Why not.
 echo "Install rust using Rustup."
@@ -285,20 +285,20 @@ if [ -z "$mac_app_login" ] ; then
     run mas signin $mac_app_login
 fi
 
-echo "Install Reeder."
-run mas install 880001334
+#echo "Install Reeder."
+#run mas install 880001334
 
-echo "Install Final Cut Pro."
-run mas install 424389933
+#echo "Install Final Cut Pro."
+#run mas install 424389933
 
-echo "Install Keynote."
-run mas install 409183694
+#echo "Install Keynote."
+#run mas install 409183694
 
-echo "Install Things."
-run mas install 407951449
+#echo "Install Things."
+#run mas install 407951449
 
-echo "Install Slack."
-run mas install 803453959
+#echo "Install Slack."
+#run mas install 803453959
 
 echo "Upgrade any Mac App Store applications."
 run mas upgrade
